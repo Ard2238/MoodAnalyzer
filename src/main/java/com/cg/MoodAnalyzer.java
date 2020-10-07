2716233a928f2db0ff86e783fde117cd5b9f98da
Ard2238 <abhishekrdas28@gmail.com>
2020-10-08 01:04:05 +0530

    Added Code to analyze and test different moods

--- src/main/java/com/cg/MoodAnalyzer.java
+++ src/main/java/com/cg/MoodAnalyzer.java
@@ -1,7 +1,23 @@
 package com.cg;
 
 public class MoodAnalyzer {
+    private String message;
+
+    public MoodAnalyzer(){
+    }
+    public MoodAnalyzer(String message){
+        this.message = message;
+    }
+
+    public String analyzeMood(){
+        if(message.contains("Sad"))
+            return "SAD";
+        return "HAPPY";
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Mood Analyzer Program");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        moodAnalyzer.analyzeMood();
     }
 }
--- src/test/java/com/cg/MoodTest.java
+++ src/test/java/com/cg/MoodTest.java
@@ -5,5 +5,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MoodTest {
+    MoodAnalyzer moodAnalyzer;
 
+    @Test
+    public void givenMessage_whenSad_returnSAD(){
+        moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        Assert.assertEquals("SAD", moodAnalyzer.analyzeMood());
+    }
+
+    @Test
+    public void givenMessage_whenAnyMood_returnHAPPY(){
+        moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+        Assert.assertEquals("HAPPY", moodAnalyzer.analyzeMood());
+    }
 }
